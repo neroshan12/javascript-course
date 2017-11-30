4b1b81c460993abe64a369f086a34adf2f46ad9e
Nero <neroshan12@gmail.com>
2017-11-30 16:39:30 +0000

    Add operators

--- script.js
+++ script.js
@@ -14,21 +14,47 @@ console.log(fullAge);
 
 // Lecture variables: 2
 
-var name = 'Nero';
-var age = 26;
-console.log(name + age); // => Nero26 - JavaScript converts the integer to a string
-console.log(age + age); // => 52 JavaScript figures out it is an integer and adds the two
+// var name = 'Nero';
+// var age = 26;
+// console.log(name + age); // => Nero26 - JavaScript converts the integer to a string
+// console.log(age + age); // => 52 JavaScript figures out it is an integer and adds the two
+//
+// var job, isMarried; // declaring a variable
+//
+// console.log(job);
+//
+// job = 'teacher'; // assigning a variable
+// isMarried = true;
+//
+// console.log(name + ' ' + age + ' ' + job + ' ' + isMarried); // => Nero 26 teacher married | all converted to a string by JavaScript
+//
+// age = 'thirty six'; // reassigning a variable
+// job = 'student';
+//
+// console.log(name + ' is a ' + age + ' year old ' + job);
+//
+// var lastName = prompt('What is the last name?');
+// console.log(lastName);
+//
+// alert(name);
 
-var job, isMarried; // declaring a variable
+// Lecture: Operators
+var now = 2016;
+var birthYear = 2017 - now;
 
-console.log(job);
+birthYear = now - 26 * 2;
+// 2017 -52
+// 1964
 
-job = 'teacher'; // assigning a variable
-isMarried = true;
+console.log(birthYear);
 
-console.log(name + ' ' + age + ' ' + job + ' ' + isMarried); // => Nero 26 teacher married | all converted to a string by JavaScript
+var ageNero = 26;
+var ageBrother = 27;
 
-age = 'thirty six'; // reassigning a variable
-job = 'student';
+ageNero = ageBrother = (3 + 5) * 4 - 6;
 
-console.log(name + ' is a ' + age + ' year old ' + job);
+ageNero++; // ageNero = ageNero + 1
+ageBrother *= 2; // ageBrother = ageBrother * 2
+
+console.log(ageNero);
+console.log(ageBrother);
